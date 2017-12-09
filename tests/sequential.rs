004e56d72492e77470d01e2b1884dc90fbbcb86f
Victor Polevoy <fx@thefx.co>
2017-12-09 11:01:01 +0300

    New test & version in cargo.toml

--- tests/sequential.rs
+++ tests/sequential.rs
@@ -15,3 +15,17 @@ fn sequential_simple() {
     assert_eq!(Test1::id().0, 0u64);
     assert_eq!(Test2::id().0, 1u64);
 }
+
+
+#[test]
+fn sequential_simple_second() {
+    use unique_type_id::{ SequentialTypeId };
+    #[derive(SequentialTypeId)]
+    struct Test1;
+    #[derive(SequentialTypeId)]
+    struct Test2;
+
+
+    assert_eq!(Test1::id().0, 2u64);
+    assert_eq!(Test2::id().0, 3u64);
+}
